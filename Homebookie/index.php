6c3b1f08c48e44e02c5b624cab78058e2d98f6e3
Guilhermemeu <167783680+Guilhermemeu@users.noreply.github.com>
2025-08-18 10:12:52 -0300

    Add files via upload

---
+++ Homebookie/index.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["id"]))
+  $_SESSION["id"] = null;
+
+$cookie_nome = "userid";
+$cookie_value = "1";
+(setcookie(name: $cookie_nome, value: $cookie_value, expires_or_options: time() + 86400, path: "/")); // 86400 = 1 dia
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1., user-scalable=no">
+  <!-- Formatação -->
+  <link rel="stylesheet" href="style.css">
+  <script src="interação.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+  <!-- Fonte -->
+  <script src="https://kit.fontawesome.com/3fed99b926.js" crossorigin="anonymous"></script>
+  <!-- JSON -->
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+  <!-- | Icone do Site | -->
+  <link icon="" href="">
+  <!-- | Titulo | -->
+  <title>HomeBook</title>
+</head>
+
+<body>
+
+
+  <?php
+  include "main.php";
+
+  if (isset($_GET['page'])) {
+
+    if ($Page == "") {
+      $Page = "Inicio";
+    } else {
+      $Page = $_GET['page'];
+    }
+    if ($Page == 'Inicio' && isset($_COOKIE) && $cookie_value == 1) {
+      include 'Template/join.php';
+    } else if ($Page == 'Inicio' && !isset($_COOKIE) && $cookie_value != 1) {
+      include '';
+    }
+
+    if ($Page == 'Sobre') {
+      include 'Template/sobre.php';
+    }
+
+
+  } else {
+    if (isset($_COOKIE) && $cookie_value == 1) {
+      include 'Template/join.php';
+    } else if (isset($_COOKIE) && $cookie_value != 1) {
+      include '';
+    }
+  }
+  ?>
+
+</body>
+
+</html>
